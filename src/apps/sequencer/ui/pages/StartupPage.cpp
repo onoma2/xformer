e2403969611a079053625bfd6f70efa1588b33d9
zinziber@gmail.com <zinziber@gmail.com>
2025-11-18 00:30:33 +0300

    __X|FORMER

--- src/apps/sequencer/ui/pages/StartupPage.cpp
+++ src/apps/sequencer/ui/pages/StartupPage.cpp
@@ -6,57 +6,56 @@
 
 #include "os/os.h"
 
-StartupPage::StartupPage(PageManager &manager, PageContext &context) :
-    BasePage(manager, context)
-{
-    _startTicks = os::ticks();
+StartupPage::StartupPage(PageManager &manager, PageContext &context)
+    : BasePage(manager, context) {
+  _startTicks = os::ticks();
 }
 
 void StartupPage::draw(Canvas &canvas) {
-    if (_state == State::Initial) {
-        _state = State::Loading;
-        _engine.suspend();
-        FileManager::task([this] () {
-            return FileManager::readLastProject(_model.project());
-        }, [this] (fs::Error result) {
-            _engine.resume();
-            _state = State::Ready;
+  if (_state == State::Initial) {
+    _state = State::Loading;
+    _engine.suspend();
+    FileManager::task(
+        [this]() { return FileManager::readLastProject(_model.project()); },
+        [this](fs::Error result) {
+          _engine.resume();
+          _state = State::Ready;
         });
-    }
+  }
 
-    if (relTime() > 1.f && _state == State::Ready) {
-        close();
-    }
+  if (relTime() > 1.f && _state == State::Ready) {
+    close();
+  }
 
-    canvas.setColor(Color::None);
-    canvas.fill();
+  canvas.setColor(Color::None);
+  canvas.fill();
 
-    canvas.setColor(Color::Bright);
+  canvas.setColor(Color::Bright);
 
-    canvas.setFont(Font::Small);
-    canvas.drawTextCentered(0, 0, Width, 32, "PEW|FORMER");
-    canvas.setFont(Font::Tiny);
-    canvas.drawTextCentered(0, 20, Width, 8, "from Phazerville with Love");
+  canvas.setFont(Font::Small);
+  canvas.drawTextCentered(0, 0, Width, 32, "__X|FORMER");
+  canvas.setFont(Font::Tiny);
+  canvas.drawTextCentered(0, 20, Width, 8, "onoma2 (Phazerville based)");
 
-    canvas.setFont(Font::Tiny);
-    canvas.drawTextCentered(0, 32, Width, 32, "LOADING ...");
+  canvas.setFont(Font::Tiny);
+  canvas.drawTextCentered(0, 32, Width, 32, "LOADING ...");
 
-    int w = std::floor(relTime() * Width);
-    canvas.fillRect(0, 32 - 2, w, 4);
+  int w = std::floor(relTime() * Width);
+  canvas.fillRect(0, 32 - 2, w, 4);
 }
 
 void StartupPage::updateLeds(Leds &leds) {
-    leds.clear();
+  leds.clear();
 
-    int progress = std::floor(relTime() * 8.f);
-    for (int i = 0; i < 8; ++i) {
-        bool active = i <= progress;
-        leds.set(MatrixMap::fromTrack(i), active, active);
-        leds.set(MatrixMap::fromStep(i), active, active);
-        leds.set(MatrixMap::fromStep(i + 8), active, active);
-    }
+  int progress = std::floor(relTime() * 8.f);
+  for (int i = 0; i < 8; ++i) {
+    bool active = i <= progress;
+    leds.set(MatrixMap::fromTrack(i), active, active);
+    leds.set(MatrixMap::fromStep(i), active, active);
+    leds.set(MatrixMap::fromStep(i + 8), active, active);
+  }
 }
 
 float StartupPage::time() const {
-    return (os::ticks() - _startTicks) / float(os::time::ms(1000));
+  return (os::ticks() - _startTicks) / float(os::time::ms(1000));
 }
