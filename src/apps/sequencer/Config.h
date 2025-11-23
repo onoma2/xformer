96c7757c50bdf92266bfff764cc2a1837477ae6b
zinziber@gmail.com <zinziber@gmail.com>
2025-11-23 22:11:26 +0300

    XFormer 0.3.0

--- src/apps/sequencer/Config.h
+++ src/apps/sequencer/Config.h
@@ -6,7 +6,7 @@
 #define CONFIG_VERSION_MAGIC            0xfadebabe
 #define CONFIG_VERSION_NAME             "__X|FORMER SEQUENCER"
 #define CONFIG_VERSION_MAJOR            0
-#define CONFIG_VERSION_MINOR            2
+#define CONFIG_VERSION_MINOR            3
 #define CONFIG_VERSION_REVISION         0
 
 // Task priorities
