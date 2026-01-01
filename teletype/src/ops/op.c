f3114f34ba984a82a8adaf932dc4c3c164e78c85
zinziber@gmail.com <zinziber@gmail.com>
2026-01-01 23:17:55 +0300

    .6.0 - Teletype removed telex ops

--- teletype/src/ops/op.c
+++ teletype/src/ops/op.c
@@ -14,7 +14,6 @@
 #include "ops/queue.h"
 #include "ops/seed.h"
 #include "ops/stack.h"
-#include "ops/telex.h"
 #include "ops/turtle.h"
 #include "ops/variables.h"
 #include "teletype_io.h"
@@ -104,50 +103,6 @@ const tele_op_t *tele_ops[E_OP__LENGTH] = {
 
     // delay
     &op_DEL_CLR,
-
-    // telex
-    &op_TO_TR, &op_TO_TR_TOG, &op_TO_TR_PULSE, &op_TO_TR_TIME, &op_TO_TR_TIME_S,
-    &op_TO_TR_TIME_M, &op_TO_TR_POL, &op_TO_KILL,
-
-    &op_TO_TR_PULSE_DIV, &op_TO_TR_PULSE_MUTE, &op_TO_TR_M_MUL,
-
-    &op_TO_M, &op_TO_M_S, &op_TO_M_M, &op_TO_M_BPM, &op_TO_M_ACT, &op_TO_M_SYNC,
-    &op_TO_M_COUNT,
-
-    &op_TO_TR_M, &op_TO_TR_M_S, &op_TO_TR_M_M, &op_TO_TR_M_BPM, &op_TO_TR_M_ACT,
-    &op_TO_TR_M_SYNC, &op_TO_TR_WIDTH, &op_TO_TR_M_COUNT,
-
-    &op_TO_CV, &op_TO_CV_SLEW, &op_TO_CV_SLEW_S, &op_TO_CV_SLEW_M,
-    &op_TO_CV_SET, &op_TO_CV_OFF, &op_TO_CV_QT, &op_TO_CV_QT_SET, &op_TO_CV_N,
-    &op_TO_CV_N_SET, &op_TO_CV_SCALE, &op_TO_CV_LOG,
-
-    &op_TO_CV_INIT, &op_TO_TR_INIT, &op_TO_INIT,
-
-    &op_TO_TR_P, &op_TO_TR_P_DIV, &op_TO_TR_P_MUTE,
-
-    &op_TO_OSC, &op_TO_OSC_SET, &op_TO_OSC_QT, &op_TO_OSC_QT_SET, &op_TO_OSC_FQ,
-    &op_TO_OSC_FQ_SET, &op_TO_OSC_N, &op_TO_OSC_N_SET, &op_TO_OSC_LFO,
-    &op_TO_OSC_LFO_SET, &op_TO_OSC_WAVE, &op_TO_OSC_SYNC, &op_TO_OSC_PHASE,
-    &op_TO_OSC_WIDTH, &op_TO_OSC_RECT, &op_TO_OSC_SLEW, &op_TO_OSC_SLEW_S,
-    &op_TO_OSC_SLEW_M, &op_TO_OSC_SCALE, &op_TO_OSC_CYC, &op_TO_OSC_CYC_S,
-    &op_TO_OSC_CYC_M, &op_TO_OSC_CYC_SET, &op_TO_OSC_CYC_S_SET,
-    &op_TO_OSC_CYC_M_SET, &op_TO_OSC_CTR,
-
-    &op_TO_ENV_ACT, &op_TO_ENV_ATT, &op_TO_ENV_ATT_S, &op_TO_ENV_ATT_M,
-    &op_TO_ENV_DEC, &op_TO_ENV_DEC_S, &op_TO_ENV_DEC_M, &op_TO_ENV_TRIG,
-    &op_TO_ENV_EOR, &op_TO_ENV_EOC, &op_TO_ENV_LOOP,
-
-    &op_TO_ENV, &op_TO_CV_CALIB, &op_TO_CV_RESET,
-
-    &op_TI_PARAM, &op_TI_PARAM_QT, &op_TI_PARAM_N, &op_TI_PARAM_SCALE,
-    &op_TI_PARAM_MAP, &op_TI_IN, &op_TI_IN_QT, &op_TI_IN_N, &op_TI_IN_SCALE,
-    &op_TI_IN_MAP, &op_TI_PARAM_CALIB, &op_TI_IN_CALIB, &op_TI_STORE,
-    &op_TI_RESET,
-
-    &op_TI_PARAM_INIT, &op_TI_IN_INIT, &op_TI_INIT,
-
-    &op_TI_PRM, &op_TI_PRM_QT, &op_TI_PRM_N, &op_TI_PRM_SCALE, &op_TI_PRM_MAP,
-    &op_TI_PRM_CALIB, &op_TI_PRM_INIT,
 
     // seed
     &op_SEED, &op_RAND_SEED, &op_SYM_RAND_SD, &op_SYM_R_SD, &op_TOSS_SEED,
